73a2f76bb30cf2c4a7631b4604ab444e1be63825
RendarCP <tjddnr0217@gmail.com>
2023-07-06 15:32:03 +0900

    Feat : 컴포넌트 정의

---
+++ src/pages/Root.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../index.css";
+
+export default function Root() {
+  return (
+    <div>
+      <div>Root</div>
+      <button>about</button>
+    </div>
+  );
+}
